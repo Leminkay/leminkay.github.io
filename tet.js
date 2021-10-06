6466cc8fb42046e3c1b49e5617859aabf663f0c1
Emil Murtazin <Leminkay@gmail.com>
2021-10-06 07:12:30 -0400

    Update tet.js

--- tet.js
+++ tet.js
@@ -1,3 +1,4 @@
 
 const html = (await (await fetch("tasks.sabantuy.cbsctf.live:45394/flag")).text()); // html as text
 const doc = new DOMParser().parseFromString(html, 'text/html');
+console.log(doc.body)
